3aa22372c5344dec8a2000f4150c41b7ef096792
zhangjinzhe <zhangjinzhe@88.com>
2022-04-12 15:55:57 +0800

    测试

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <el-button @click="$router.push('/edit/diy')">建造组件</el-button>
+<!--    <el-button @click="$router.push('/edit/diy')">建造组件</el-button>-->
     <el-button @click="$router.push('/edit/module')">建造页面</el-button>
     <el-button @click="$router.push('/edit/system')">建造其他页面</el-button>
   </div>
